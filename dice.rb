c2c633b8330623b8ded64c49fb448db3c75e3210
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-10-24 17:13:17 +0000

    Added Navigation Pane and Layout erb

--- dice.rb
+++ dice.rb
@@ -36,7 +36,7 @@ get("/dice/2/6") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_six, { :layout => :wrapper })
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -44,20 +44,18 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  erb(:two_ten)
 
 end
 
 get("/dice/1/20") do
-  first_die = rand(1..20)
+  @die = rand(1..20)
 
-  outcome = "You rolled a #{first_die}"
+  @outcome = "You rolled a #{@die}"
 
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  erb(:one_twenty)
 
 end
 
@@ -69,10 +67,9 @@ get("/dice/5/4") do
   fifth_die = rand(1..4)
 
   sum = first_die + second_die + third_die + fourth_die + fifth_die
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total sum #{sum}."
+  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total sum #{sum}."
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
 
 
 
